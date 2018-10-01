9e86296be99d2ef6fded474bb3a37feb0824ceb8
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-01 17:56:44 +0000

    Created a file, ninjas.js and nested that inside the root app.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Ninjas from './Ninjas';  //Imported Ninjas in order to nest
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> My first React App </h1>
+        <p> Welcome! </p>
+        <Ninjas /> 
       </div>
     );
   }
