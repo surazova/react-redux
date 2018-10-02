5ee9e7907ce372efc17bf6b184429cbcf97b3711
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-02 03:42:49 +0000

    Created a functional component, stateless (UI)

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 
-class Ninjas extends Component{
-    render(){
-        const { ninjas } = this.props;  //Also set a unique key id for key={ninja.id}
-        const ninjaList = ninjas.map(ninja => { //cycle through ninjas array, mapping it to a new array, and returning and template for that ninja
+//changin into a functional component, deleted render()
+const Ninjas = ({ninjas}) => { //taking orops as a parameter (previous: const { ninjas } = props;)
+        const ninjaList = ninjas.map(ninja => { 
             return(
             <div className="ninja" key={ninja.id}> 
             <div>Name: {ninja.name}</div> 
@@ -18,6 +17,5 @@ class Ninjas extends Component{
         </div>
         )
     }
-}
 
 export default Ninjas
--- notes/notes.md
+++ notes/notes.md
@@ -1,3 +1,14 @@
-Props </br>
-A way for us to pass data from the parent component (App.js) to the child component (Ninjas.js) </br>
-Because these components are not hard coded, they can be reused </br>
+Container vs UI Components </br>
+</br>
+Container Components </br>
+-Contain state </br>
+-Contain lifecycle hooks </br>
+-Not concerned with UI </br>
+-Use classes to create </br>
+
+</br>
+UI Components </br>
+-Don't contain state </br>
+-Receive data from props </br>
+-Only concerned with UI </br>
+-Use functions to create </br>
