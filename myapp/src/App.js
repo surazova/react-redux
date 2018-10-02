f688418c12ec12077ed898ae4958e2bdc4320582
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-02 22:11:32 +0000

    Used the spread operator to copy and add element, without alterint the set state

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas';  //Imported Ninjas in order to nest
+import Ninjas from './Ninjas';  
 import AddNinja from './AddNinja'
 class App extends Component {
   
@@ -10,13 +10,23 @@ class App extends Component {
         { name: 'Crystal', age: 25, belt: 'pink', id: 3}
         ]
     }
+    
+    //Creating a new function and prop, so the funciton can be called from AddNinja.js 
+    addNinja = (ninja) => {
+      //console.log(ninja);
+      ninja.id=Math.random(); //gives random id that is unique 
+      let ninjas = [...this.state.ninjas, ninja] //spread operator, used to copy the original array, then adding the element to the array so that you are not changing the set state/altering the original. 
+      this.setState({
+        ninjas: ninjas
+      })
+    }
     render() {
     return (
       <div className="App">
         <h1> My first React App </h1>
         <p> Welcome! </p>
         <Ninjas ninjas={this.state.ninjas}/> 
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
