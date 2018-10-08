f47d66b23dbe1cd60079d67286caeed1d039aab2
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-08 15:33:52 +0000

    imported Link and NavLink so that additional requests to the server are not made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import { Link, NavLink } from 'react-router-dom' //Changes a href to link to. Link tags prevent the default action of sending a request to the server, makes the app smoother and quicker
+                                                //NavLink gives you an active tag when that link is clicked 
 const Navbar = () => {
     return (
-        <nav className="nav-wrapper red darken-3">
+        <nav className="nav-wrapper teal accent-3">
             <div className="container"> 
                 <a href=".eslint" className="brand-logo">Poketimes</a>
                 
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
 
                 </ul>
             </div>
