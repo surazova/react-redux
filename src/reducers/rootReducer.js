35b9700f2ef49c39aec9d10f1fc649296c929ac2
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-11 02:49:42 +0000

    Importing a connect functionand applying a post property to our props

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,10 @@
 const initState = {
-    posts: []
+    posts: [
+        { id: '1', title: 'Squirtle laid an egg', body: 'Lorem ipsum, dolor sit amet' },
+        { id: '2', title: 'Charmander laid an egg', body: 'Lorem ipsum, dolor sit amet' },
+        { id: '3', title: 'a Helix Fossil was found', body: 'Lorem ipsum, dolor sit amet' },
+
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
